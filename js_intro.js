7762351a5f435ee3739ae28468880d882f62ec4b
Aleiah Aguilar <41592257+Aleiah@users.noreply.github.com>
2018-08-09 15:33:09 -0500

    Add files via upload
    uploading 
    inline JS HTML
    functions JS HTML
    JS intro.js
    order-pizza HTML

---
+++ js_intro.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var myAge = prompt("How old are you?");
+
+function birthday() {
+    var message = "Happy Birthday! You are ";
+    message += ++myAge;
+    message += " years old.";
+    console.log(message);
+}
+
+
+
+//
+// var message1 = "Happy Birthday! You are ";
+// message1 += ++age;
+// message1 += " years old.";
+// console.log(message1);
+//
+birthday();
+//
+// var message2 = "Happy Birthday! You are ";
+// message2 += ++age;
+// message2 += " years old.";
+// console.log(message2);
+//
+birthday();
+//
+// var message3 = "Happy Birthday! You are ";
+// message3 += ++age;
+// message3 += " years old.";
+// console.log(message3);
+//
+birthday();
+//
+// var message4 = "Happy Birthday! You are ";
+// message4 += ++age;
+// message4 += " years old.";
+// console.log(message4);
+birthday();
