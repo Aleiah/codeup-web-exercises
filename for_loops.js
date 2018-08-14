20c8b4cdedfd92e812b1d0d3addb3f226c444aa7
Aleiah Aguilar <aleiah.aguilar1@gmail.com>
2018-08-14 08:39:44 -0500

    loops!!

--- for_loops.js
+++ for_loops.js
@@ -1,41 +1,41 @@
-// function showMultiplicationsTable(number) {
-    // for(var i = 1; i <= 10; i++) {
-//         console.log(number + " x " + i + " = " + (number * i));
-//     }
-// }
+function showMultiplicationsTable(number) {
+    for(var i = 1; i <= 10; i++) {
+        console.log(number + " x " + i + " = " + (number * i));
+    }
+}
+
+showMultiplicationsTable(5);
+showMultiplicationsTable(20);
+
+
 //
-// showMultiplicationsTable(5);
-// showMultiplicationsTable(20);
+
+var message;
+var number;
+for(var i = 0; i < 10; i++) {
+    number = Math.floor(Math.random() * (200 - 20)) + 20;
+    message = "";
+    if(number % 2 === 0) {
+        message = " is even";
+    } else {
+        message = " is odd";
+    }
+
+    console.log(number + message);
+}
+
 //
+var output = "";
+for(var i = 1; i <= 9; i++) {
+    output = ""; }
+    for(var j = 1; j <= i; j++) {
+        output += i;
+    }
+        console.log(output)
+}
+
 //
-// //
-//
-// var message;
-// var number;
-// for(var i = 0; i < 10; i++) {
-//     number = Math.floor(Math.random() * (200 - 20)) + 20;
-//     message = "";
-//     if(number % 2 === 0) {
-//         message = " is even";
-//     } else {
-//         message = " is odd";
-//     }
-//
-//     console.log(number + message);
-// }
-//
-// //
-// var output = "";
-// for(var i = 1; i <= 9; i++) {
-//     output = ""; }
-//     for(var j = 1; j <= i; j++) {
-//         output += i;
-//     }
-//         console.log(output)
-// }
-//
-// //
-var i = 5;
-for(var i = 5; i <= 100; i--) {
-    console.log(i--);
+
+for(var i = 100; i >= 5; i-5) {
+    console.log(i);
 }
